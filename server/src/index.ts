729a6cb2e20a08c61ed7957af1f030dd386d4979
gregorylister <greg.lister@iontag.com>
2018-03-11 22:00:55 +1000

    Removed nav + static router and added proxy

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,9 @@
 import * as express from "express";
 import * as path from "path";
-import navRouter from "./router/navRouter";
 import tagRouter from "./router/tagRouter";
 
 const app = express();
-app.use(express.static(path.join(__dirname, "public"), { etag: true }));
 app.use(tagRouter);
-app.use(navRouter);
 
 const port = process.env.PORT || 3001;
 app.set("port", port);

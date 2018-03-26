9dbe7c2528b2c971237ffcbc65b76a890357311b
gregorylister <greg.lister@iontag.com>
2018-03-26 22:52:25 +1000

    Added URLS

--- server/src/router/Urls.ts
+++ server/src/router/Urls.ts
@@ -1,2 +1,8 @@
 export const TAG_ADD = "/tag/add";
 export const TAG_GET = "/tag/get";
+
+export const SERVICE_TAG_ADD = "/servicetag/add";
+export const SERVICE_TAG_GET = "/servicetag/get";
+
+export const INSPECTION_TAG_ADD = "/inspectiontag/add";
+export const INSPECTION_TAG_GET = "/inspectiontag/get";

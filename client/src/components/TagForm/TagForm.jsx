82f90c974e298be43d6898bc2570732105be25c3
gregorylister <greg.lister@iontag.com>
2018-04-12 14:16:01 +1000

    Updated webconfig for deployment + more work on add tag form

--- client/src/components/TagForm/TagForm.jsx
+++ client/src/components/TagForm/TagForm.jsx
@@ -13,59 +13,99 @@ class TagForm extends React.Component
     constructor(props)
     {
         super(props);
-        this.postData = this.postData.bind(this);
+        this.state = {
+            tag_code: "",
+            tag_number: "",
+            item_name: "",
+            item_number: "",
+            location: "",
+            general_comments: "",
+            last_modified: new Date()
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
-    async postData()
+    onChange(event)
     {
-        await fetch("/tag/add", {
-            method: "post",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                tag_code: "test",
-                tag_number: "1"
-            })
-        });
+        const state = this.state;
+        state[event.target.name] = event.target.value;
+        this.setState(state);
+    }
+
+    async handleSubmit(event)
+    {
+        try
+        {
+            event.preventDefault();
+            const {tag_code, tag_number, item_name, item_number, location, general_comments} = this.state;
+
+            await fetch("/tag/add", {
+                method: "post",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    tag_code: tag_code,
+                    tag_number: tag_number,
+                    item_name: item_name,
+                    item_number: item_number,
+                    location: location,
+                    general_comments: general_comments,
+                    last_modified: new Date()
+                })
+            });
+        }
+        catch (err)
+        {
+            console.error(err);
+        }
     }
 
     render()
     {
+        const {tag_code, tag_number, item_name, item_number, location, general_comments} = this.state;
         return (
             <Modal className={modalWidth} size="lg" isOpen={this.props.isOpen} toggle={this.props.toggle}>
                 <ModalHeader className="justify-content-center" toggle={this.props.toggle}>
                     Add Tag
                 </ModalHeader>
                 <ModalBody>
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <FormInputs
                             ncols = {["col-md-4 pr-1" , "col-md-4 px-1" , "col-md-4 pl-1"]}
                             proprieties = {[
                                 {
                                     label : "ID",
                                     inputProps : {
+                                        name: "id",
                                         type : "text",
                                         disabled: true,
                                         defaultValue: "",
-                                        placeholder: ""
+                                        placeholder: "",
                                     }
                                 },
                                 {
                                     label : "Code",
                                     inputProps : {
+                                        name: "tag_code",
                                         type : "text",
                                         defaultValue: "",
-                                        placeholder: ""
+                                        placeholder: "",
+                                        value: tag_code,
+                                        onChange: this.onChange
                                     }
                                 },
                                 {
                                     label : "Number",
                                     inputProps : {
+                                        name: "tag_number",
                                         type : "text",
                                         defaultValue: "",
-                                        placeholder: ""
+                                        placeholder: "",
+                                        value: tag_number,
+                                        onChange: this.onChange
                                     }
                                 }
                             ]}
@@ -76,25 +116,34 @@ class TagForm extends React.Component
                                 {
                                     label : "Item Name",
                                     inputProps : {
+                                        name: "item_name",
                                         type : "text",
                                         defaultValue: "",
-                                        placeholder: ""
+                                        placeholder: "",
+                                        value: item_name,
+                                        onChange: this.onChange
                                     }
                                 },
                                 {
                                     label : "Item Number",
                                     inputProps : {
+                                        name: "item_number",
                                         type : "text",
                                         defaultValue: "",
-                                        placeholder: ""
+                                        placeholder: "",
+                                        value: item_number,
+                                        onChange: this.onChange
                                     }
                                 },
                                 {
                                     label : "Location",
                                     inputProps : {
+                                        name: "location",
                                         type : "text",
                                         defaultValue: "",
-                                        placeholder: ""
+                                        placeholder: "",
+                                        value: location,
+                                        onChange: this.onChange
                                     }
                                 }
                             ]}
@@ -105,11 +154,14 @@ class TagForm extends React.Component
                                 {
                                     label : "Comments",
                                     inputProps : {
+                                        name: "general_comments",
                                         type : "textarea",
                                         rows: "4",
                                         cols: "80",
                                         defaultValue: "",
-                                        placeholder: "Enter comments here"
+                                        placeholder: "Enter comments here",
+                                        value: general_comments,
+                                        onChange: this.onChange
                                     }
                                 }
                             ]}
@@ -117,7 +169,7 @@ class TagForm extends React.Component
                     </form>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={this.postData}>
+                    <Button color="primary" onClick={this.handleSubmit}>
                         Submit
                     </Button>
                     <Button color="danger" onClick={this.props.toggle}>

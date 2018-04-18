3e54c532365832ad442ccf71a05c0dd008bcc616
gregorylister <greg.lister@iontag.com>
2018-04-18 22:58:31 +1000

    Updated routes

--- client/src/routes/dashboard.jsx
+++ client/src/routes/dashboard.jsx
@@ -6,11 +6,21 @@ var dashRoutes =
 [
     { path: "/dashboard", name: "Dashboard", icon: "design_app", component: Dashboard },
 
-    { collapse: true, path: "/tagsettings", name: "Tag Settings", state: "openTags", icon: "shopping_tag-content",
+    {
+        collapse: true,
+        path: "/taginfo",
+        name: "Tag Info",
+        state: "openTags",
+        icon: "shopping_tag-content",
         views:
-            [
-                { path: "/tagsettings/tagtable", name: "Tag Table", mini: "TT", component: TagTableView }
-            ]
+        [
+            {
+                path: "/taginfo/tags",
+                name: "Tags",
+                mini: "TA",
+                component: TagTableView
+            }
+        ]
     },
 
     { path: "/calendar", name: "Calendar", icon: "media-1_album", component: Calendar },
